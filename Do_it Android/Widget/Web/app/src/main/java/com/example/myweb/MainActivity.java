7600ef25cbada2f511931c3be215cad16ff833b5
종찬 <ieejo716@naver.com>
2021-01-19 23:03:44 +0900

    웹뷰 사용하기

---
+++ Do_it Android/Widget/Web/app/src/main/java/com/example/myweb/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myweb;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit1);
+        button = findViewById(R.id.button);
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl(editText.getText().toString());
+            }
+        });
+    }
+}
