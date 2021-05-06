aa7574b01934e5064294836e461ebe761ecc52e8
Chan <ieejo716@naver.com>
2021-05-06 17:21:40 +0900

    변수 선언

--- BasicApp/BMI_Calulator/app/src/main/java/com/example/bmi_calulator/MainActivity.kt
+++ BasicApp/BMI_Calulator/app/src/main/java/com/example/bmi_calulator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.bmi_calulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText : EditText = findViewById(R.id.height)
+        val weightEditText : EditText = findViewById(R.id.weight)
+        val resultButton : Button = findViewById(R.id.button)
+
+
     }
 }
