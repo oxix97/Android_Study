77113e1603d8d8b7bedcf29d43177f147ca8535f
Chan <ieejo716@naver.com>
2021-05-07 16:26:32 +0900

    BMI 계산기

--- BasicApp/BMI_Calulator/app/src/main/java/com/example/bmi_calulator/MainActivity.kt
+++ BasicApp/BMI_Calulator/app/src/main/java/com/example/bmi_calulator/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.bmi_calulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val heightEditText : EditText = findViewById(R.id.height)
-        val weightEditText : EditText = findViewById(R.id.weight)
-        val resultButton : Button = findViewById(R.id.button)
+        val heightEditText: EditText = findViewById(R.id.height)
+        val weightEditText: EditText = findViewById(R.id.weight)
+        val resultButton: Button = findViewById(R.id.button)
 
+        resultButton.setOnClickListener {
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight = weightEditText.text.toString().toInt()
+
+            val intent = Intent(this,ResultActivity::class.java)
+
+            intent.putExtra("height",height)
+            intent.putExtra("weight",weight)
+
+            startActivity(intent)
+        }
     }
 }
