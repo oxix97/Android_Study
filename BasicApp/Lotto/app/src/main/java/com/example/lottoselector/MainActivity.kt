680bad05bf2e0fb5c5843ccf77ff17c9477669e3
Chan <ieejo716@naver.com>
2021-05-08 22:09:58 +0900

    05-08 로또 생성기 로직

--- BasicApp/Lotto/app/src/main/java/com/example/lottoselector/MainActivity.kt
+++ BasicApp/Lotto/app/src/main/java/com/example/lottoselector/MainActivity.kt
@@ -2,10 +2,121 @@ package com.example.lottoselector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton: Button by lazy {
+        findViewById(R.id.reset)
+    }
+
+    private val addButton: Button by lazy {
+        findViewById(R.id.addButton)
+    }
+
+    private val runButton: Button by lazy {
+        findViewById(R.id.runButton)
+    }
+
+    private val numberPicker: NumberPicker by lazy {
+        findViewById(R.id.numberPicker)
+    }
+
+    private val pickNumberSet = hashSetOf<Int>()
+    private var didRun = false
+
+    private val numberTextViewList: List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById(R.id.textView1),
+            findViewById(R.id.textView2),
+            findViewById(R.id.textView3),
+            findViewById(R.id.textView4),
+            findViewById(R.id.textView5),
+            findViewById(R.id.textView6)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        initRunButton()
+        initAddButton()
+        initClearButton()
     }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+            didRun = false
+        }
+    }
+
+    private fun initAddButton() {
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumberSet.size >= 5) {
+                Toast.makeText(this, "번호 개수는 5개까지만 선택 가능합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumberSet.contains(numberPicker.value)) {
+                Toast.makeText(this, "이미 선택한 번호 입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val textView = numberTextViewList[pickNumberSet.size]
+            textView.isVisible = true
+            textView.text = numberPicker.value.toString()
+
+            pickNumberSet.add(numberPicker.value)
+
+        }
+    }
+
+    private fun initRunButton() {
+        runButton.setOnClickListener {
+            val list = getRandomNumber()
+            didRun = true
+            list.forEachIndexed{
+                index, number -> val textView = numberTextViewList[index]
+                textView.text = number.toString()
+                textView.isVisible = true
+            }
+        }
+    }
+
+    private fun getRandomNumber(): List<Int> {
+        val numberList = mutableListOf<Int>().apply {
+            for (i in 1..45) {
+                if (pickNumberSet.contains(i)) {
+                    continue
+                }
+                this.add(i)
+            }
+        }
+        numberList.shuffle()
+
+        val newList = pickNumberSet.toList() + numberList.subList(0,6 - pickNumberSet.size)
+        return newList.sorted()
+    }
+
 }
+
+
+
+
