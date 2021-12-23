70a48dbd6ab70e97a9a061bd856d8422477eb6dd
Chan <ieejo716@naver.com>
2021-12-23 18:52:56 +0900

    Android_Spinner

---
+++ Android_Kotlin/chapter5/app/src/main/java/com/example/chapter5/SpinnerActivity.kt
@@ -0,0 +1,36 @@
+package com.example.chapter5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.chapter5.databinding.ActivitySpinnerBinding
+
+class SpinnerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySpinnerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySpinnerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var data = listOf("1", "2", "3", "4", "5")
+        var adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, data)
+        binding.spSpinner.adapter = adapter
+
+        binding.spSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                binding.tvResult.text = data.get(position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+    }
+}
