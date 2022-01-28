4e34007dc7324cf17a0084c3ca0d31e593d90590
username <ieejo716@naver.com>
2022-01-28 23:42:36 +0900

    이것이 안드로이드 chapter6 BaseActivity

--- Android_Kotlin/chapter6/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Android_Kotlin/chapter6/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,60 +1,45 @@
 package com.example.myapplication
 
-import android.content.pm.PackageManager
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import com.example.myapplication.base.BaseActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
+    override fun permissionGranted(requestCode: Int) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 99)
+    }
+
+    override fun permissionDenied(requestCode: Int) {
+        Toast.makeText(this, "reject", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.btnCameraAccess.setOnClickListener {
-            checkPermission()
+            requirePermission(arrayOf(Manifest.permission.CAMERA), 10)
         }
 
         setContentView(binding.root)
     }
 
-    private fun checkPermission() {
-        val cameraPermission =
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-        if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
-            startProcess()
-        } else {
-            requestPermission()
-        }
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 99)
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            99 -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startProcess()
-                } else {
-                    finish()
-                }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.d("camera", "success")
+            } else {
+                Log.d("camera", "fail")
             }
         }
     }
-
-    private fun startProcess() {
-        Toast.makeText(this, "승인", Toast.LENGTH_SHORT).show()
-    }
-
 }
