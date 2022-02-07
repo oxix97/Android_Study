78cb8c294b88662290dc4a78b5b9c4929b812296
username <ieejo716@naver.com>
2022-02-08 01:11:09 +0900

    android setting

---
+++ Android_Kotlin/chapter8/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.example.myapplication.base.BaseViewUtil
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity :
+    BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    override fun initView() {
+
+    }
+}
