ab964dcf9158c2b6d35e2e82122408a402cd9744
username <ieejo716@naver.com>
2022-02-21 22:48:21 +0900

    init setting

---
+++ Intermediate/PushAlarmReceiver/app/src/main/java/com/example/pushalarm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pushalarm
+
+import android.os.Bundle
+import com.example.pushalarm.base.baseutil.BaseViewUtil
+import com.example.pushalarm.databinding.ActivityMainBinding
+
+class MainActivity :
+    BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
