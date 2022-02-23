dfba0b73cd2196f7abf2e89a4c3823773b30ce45
username <ieejo716@naver.com>
2022-02-24 00:17:41 +0900

    cloud messaging

--- Intermediate/PushAlarmReceiver/app/src/main/java/com/example/pushalarm/MainActivity.kt
+++ Intermediate/PushAlarmReceiver/app/src/main/java/com/example/pushalarm/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.pushalarm
 
 import android.os.Bundle
+import android.util.Log
 import com.example.pushalarm.base.baseutil.BaseViewUtil
 import com.example.pushalarm.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
-
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if (it.isSuccessful) {
+                binding.tvMainToken.setText("${it.result}")
+                Log.d("token",it.result.toString())
+            }
+        }
     }
 }
