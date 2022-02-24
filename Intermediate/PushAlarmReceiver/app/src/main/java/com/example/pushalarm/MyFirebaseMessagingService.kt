2186550020f7a58ee75723624cf7c064d5700b85
username <ieejo716@naver.com>
2022-02-24 15:45:29 +0900

    Notification

--- Intermediate/PushAlarmReceiver/app/src/main/java/com/example/pushalarm/MyFirebaseMessagingService.kt
+++ Intermediate/PushAlarmReceiver/app/src/main/java/com/example/pushalarm/MyFirebaseMessagingService.kt
@@ -1,5 +1,12 @@
 package com.example.pushalarm
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -11,5 +18,62 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        createNotificationChannel()
+
+        val type = message.data["type"]?.let {
+            NotificationType.valueOf(it)
+        }
+        val title = message.data["title"]
+        val text = message.data["message"]
+
+        type ?: return
+
+        NotificationManagerCompat.from(this)
+            .notify(type.id, createNotification(type, title, text))
+
+    }
+
+    private fun createNotification(
+        type: NotificationType?,
+        title: String?,
+        text: String?
+    ): Notification {
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic__90107_alarm_alert_bell_christmas_notification_icon)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        when (type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+
+            }
+            NotificationType.CUSTOM ->{
+
+            }
+            null -> TODO()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_NAME = "TEST"
+        const val CHANNEL_DESCRIPTION = "test 채널"
+        const val CHANNEL_ID = "channel_id"
     }
 }
