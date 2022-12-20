d1f44839ddf40b8c8f50186cd13deae56598b0a0
username <ieejo716@naver.com>
2022-12-20 23:11:52 +0900

    [FEAT] : create channel

--- Android_Kotlin/FirebaseMessagingService/app/src/main/java/com/example/pushalarmservice/MyFirebaseMessagingService.kt
+++ Android_Kotlin/FirebaseMessagingService/app/src/main/java/com/example/pushalarmservice/MyFirebaseMessagingService.kt
@@ -1,5 +1,10 @@
 package com.example.pushalarmservice
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,5 +18,26 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        createNotification()
+
+    }
+
+    private fun createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "채널 1"
+        private const val CHANNEL_DESCRIPTION = "채널 1 입니다."
+        private const val CHANNEL_ID = "channel id"
     }
 }
